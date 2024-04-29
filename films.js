83ef8336ec7540a4546e0097d35542c4ad7af7d4
Banajit Rajbongshi <64951250+Benz404@users.noreply.github.com>
2024-04-29 20:36:27 +0530

    one time upload

---
+++ films.js
@@ -0,0 +1,22 @@
+autoPlayYouTubeModal();
+
+//FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
+function autoPlayYouTubeModal() {
+    var trigger = $("body").find('[data-toggle="modal"]');
+    trigger.click(function () {
+        var theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-theVideo"),
+            videoSRCauto = videoSRC + "?autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRC);
+        });
+    });
+}
+// ////////////////////////////////////////////////////////////////////////////////////////
+document.getElementById('scrollDown').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.querySelector('#first1').scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
